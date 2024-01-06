fc29408910b9896df250896a2601d1d591f35f63
Kunj Parikh <88554657+Kunj-Parikh@users.noreply.github.com>
2024-01-05 19:39:16 -0500

    arrow functions plus triple equals

--- script.js
+++ script.js
@@ -12,37 +12,13 @@ let angle2 = 0;
 let angle3 = 0;
 let angle4 = 0;
 let angle5 = 0;
-function rotatShip1() {
-    angle1 = angle1 == 0 ? 90 : 0
-    ship1.style.transform = `rotate(${angle1}deg)`
-}
-
-function rotatShip2() {
-    angle2 = angle2 == 0 ? 90 : 0
-    ship2.style.transform = `rotate(${angle2}deg)`
-}
-
-function rotatShip3() {
-    angle3 = angle3 == 0 ? 90 : 0
-    ship3.style.transform = `rotate(${angle3}deg)`
-}
-
-function rotatShip4() {
-    angle4 = angle4 == 0 ? 90 : 0
-    ship4.style.transform = `rotate(${angle4}deg)`
-}
-
-function rotatShip5() {
-    angle5 = angle5 == 0 ? 90 : 0
-    ship5.style.transform = `rotate(${angle5}deg)`
-}
 
 function rotat_all() {
-    angle1 = angle1 == 0 ? 90 : 0
-    angle2 = angle2 == 0 ? 90 : 0
-    angle3 = angle3 == 0 ? 90 : 0
-    angle4 = angle4 == 0 ? 90 : 0
-    angle5 = angle5 == 0 ? 90 : 0
+    angle1 = angle1 === 0 ? 90 : 0
+    angle2 = angle2 === 0 ? 90 : 0
+    angle3 = angle3 === 0 ? 90 : 0
+    angle4 = angle4 === 0 ? 90 : 0
+    angle5 = angle5 === 0 ? 90 : 0
     angles = [angle1, angle2, angle3, angle4, angle5]
     const shipArr = Array.from(shipContainer.children)
     for(let i=0; i<5; i++) {
@@ -52,8 +28,23 @@ function rotat_all() {
 }
 
 rotateButton.addEventListener('click', rotat_all)
-ship1.addEventListener('click', rotatShip1)
-ship2.addEventListener('click', rotatShip2)
-ship3.addEventListener('click', rotatShip3)
-ship4.addEventListener('click', rotatShip4)
-ship5.addEventListener('click', rotatShip5)
+ship1.addEventListener('click', ()=> {
+    angle1 = angle1 == 0 ? 90 : 0
+    ship1.style.transform = `rotate(${angle1}deg)`
+})
+ship2.addEventListener('click', ()=> {
+    angle2 = angle2 == 0 ? 90 : 0
+    ship2.style.transform = `rotate(${angle2}deg)`
+})
+ship3.addEventListener('click', ()=> {
+    angle3 = angle3 == 0 ? 90 : 0
+    ship3.style.transform = `rotate(${angle3}deg)`
+})
+ship4.addEventListener('click', ()=> {
+    angle4 = angle4 == 0 ? 90 : 0
+    ship4.style.transform = `rotate(${angle4}deg)`
+})
+ship5.addEventListener('click', () => {
+    angle5 = angle5 == 0 ? 90 : 0
+    ship5.style.transform = `rotate(${angle5}deg)`
+})
