8974f776b080c387572fccfae5592134c476ad5f
bz421 <bozhan.harry@gmail.com>
2024-01-08 15:14:28 -0500

    1/8/2024 Period 8

--- script.js
+++ script.js
@@ -1,30 +1,48 @@
-const shipContainer = document.querySelector('.ship-container');
-const rotateButton = document.querySelector('#rotate-button'); 
+const gameContainer = document.getElementById("game-container")
+const shipContainer = document.querySelector('.ship-container')
+const rotateButton = document.querySelector('#rotate-button')
 const shipArr = Array.from(shipContainer.children)
 let angle = [0, 0, 0, 0, 0]
 
 function rotat_all() {
     angle.forEach((e, i, arr) => arr[i] = arr[i] === 0 ? 90 : 0)
-    for(let i=0; i<5; i++) {
+    for (let i = 0; i < 5; i++) {
         shipArr[i].style.transform = `rotate(${angle[i]}deg)`
     }
 
 }
+const side = 10;
+function createBoard(color, user) {
+    const board = document.createElement('div')
+    board.classList.add('boards')
+    board.style.backgroundColor = color
+    board.id = user
+    for(let i = 0; i<side*side; i++) {
+        const block = document.createElement('div')
+        block.classList.add('block')
+        block.id = i
+        board.append(block)
+    }
+    gameContainer.append(board)
+}
+
+createBoard('gray', 'p')
+createBoard('green', 'c')
 
 rotateButton.addEventListener('click', rotat_all)
-shipArr[0].addEventListener('click', ()=> {
+shipArr[0].addEventListener('click', () => {
     angle[0] = angle[0] == 0 ? 90 : 0
     shipArr[0].style.transform = `rotate(${angle[0]}deg)`
 })
-shipArr[1].addEventListener('click', ()=> {
+shipArr[1].addEventListener('click', () => {
     angle[1] = angle[1] == 0 ? 90 : 0
     shipArr[1].style.transform = `rotate(${angle[1]}deg)`
 })
-shipArr[2].addEventListener('click', ()=> {
+shipArr[2].addEventListener('click', () => {
     angle[2] = angle[2] == 0 ? 90 : 0
     shipArr[2].style.transform = `rotate(${angle[2]}deg)`
 })
-shipArr[3].addEventListener('click', ()=> {
+shipArr[3].addEventListener('click', () => {
     angle[3] = angle[3] == 0 ? 90 : 0
     shipArr[3].style.transform = `rotate(${angle[3]}deg)`
 })
